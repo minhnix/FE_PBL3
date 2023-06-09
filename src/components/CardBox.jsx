e5efa573f8d96eee2ab0bdfb769fc3176a52a4fd
Vinh <lhv.vinh88@gmail.com>
2023-06-09 16:37:04 +0700

    fix css

--- src/components/CardBox.jsx
+++ src/components/CardBox.jsx
@@ -22,34 +22,70 @@ function CardBox(props) {
   return (
     <Card
       className={props.type ? "info" : "items"}
-      style={{ width: "18rem", boxShadow: "7px 4px 4px 0 rgba(0,0,0,0.25)" }}
+      style={{
+        width: "18rem",
+        boxShadow: "7px 4px 4px 0 rgba(0,0,0,0.25)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "between",
+      }}
     >
       <Card.Img
+        style={{ height: "280px", objectFit: "contain", flexShrink: 0 }}
         variant="top"
         src={props.menu?.imageUrl ? props.menu?.imageUrl : "Card1.png"}
       />
-      <Card.Body>
+      <Card.Body style={{ height: "calc(100 % -280px)" }}>
         {props.type === "info" ? (
           <Card.Text style={{ textAlign: "center" }}>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
         ) : (
-          <Card.Text style={{ textAlign: "center" }}>
-            <h3>{props.menu?.name}</h3>
-            <select
-              name=""
-              id=""
-              onChange={(e) => {
-                setSize(e.target.value);
+          <Card.Text
+            style={{
+              textAlign: "center",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flex: 1,
               }}
             >
-              <option value="SIZE_S">S</option>
-              <option value="SIZE_M">M</option>
-              <option value="SIZE_L">L</option>
-              <option value="SIZE_XL">XL</option>
-            </select>
-            <h4>Prices: {getPrice(props.menu.cost, size)} VNĐ</h4>
+              <h3>{props.menu?.name}</h3>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                // marginTop: "auto",
+                flexShrink: 0,
+              }}
+            >
+              <select
+                style={{
+                  width: "40px",
+                  textAlign: "center",
+                  alignItems: "center",
+                  margin: "auto",
+                }}
+                name=""
+                id=""
+                onChange={(e) => {
+                  setSize(e.target.value);
+                }}
+              >
+                <option value="SIZE_S">S</option>
+                <option value="SIZE_M">M</option>
+                <option value="SIZE_L">L</option>
+                <option value="SIZE_XL">XL</option>
+              </select>
+              <h4>Prices: {getPrice(props.menu.cost, size)} VNĐ</h4>
+            </div>
           </Card.Text>
         )}
       </Card.Body>
